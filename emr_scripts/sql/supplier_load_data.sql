30f2cadf20a64f1c5a6e4a38325ac52c38b9ca70
Debo Mukherjee <debo.mukherjee@nielsen.com>
2020-06-04 15:52:17 -0500

    Changes

--- emr_scripts/sql/supplier_load_data.sql
+++ emr_scripts/sql/supplier_load_data.sql
@@ -1,23 +1,26 @@
 truncate table :schema_name.supplier_stage;
 
-copy :schema_name.supplier_stage
-from :emr_file_path credentials
-iam_role :iam_role
-delimiter '|' gzip;
+select * from :schema_name.supplier_data limit 10;
 
 
-insert into :schema_name.supplier_data
-(
- select
-     s_suppkey ,
-        s_name ,
-        s_address ,
-        s_nationkey ,
-        s_phone ,
-        s_acctbal ,
-        s_comment,
-        current_timestamp as load_ts
-      from :schema_name.supplier_stage
-);
+--copy :schema_name.supplier_stage
+--from :emr_file_path credentials
+--iam_role :iam_role
+--delimiter '|' gzip;
+--
+--
+--insert into :schema_name.supplier_data
+--(
+-- select
+--     s_suppkey ,
+--        s_name ,
+--        s_address ,
+--        s_nationkey ,
+--        s_phone ,
+--        s_acctbal ,
+--        s_comment,
+--        current_timestamp as load_ts
+--      from :schema_name.supplier_stage
+--);
 
 
