22150efaf9fecb8ea2011139b9e76d7d5817a926
Debo Mukherjee <debo.mukherjee@nielsen.com>
2020-06-08 00:59:58 -0500

    emr changes

--- emr_scripts/sql/supplier_load_data.sql
+++ emr_scripts/sql/supplier_load_data.sql
@@ -2,9 +2,14 @@ BEGIN transaction;
 
 truncate table :schema_name.supplier_stage;
 
-copy :schema_name.supplier_stage
-from 'emr://j-35YPEDUMMF9AO/output_store/supplier/20200608/'
-credentials iam_role 'arn:aws:iam::048532184061:role/aws-assignment-redshift-s3-access-role'
+--copy :schema_name.supplier_stage
+--from 'emr://j-35YPEDUMMF9AO/output_store/supplier/20200608/'
+--credentials iam_role 'arn:aws:iam::048532184061:role/aws-assignment-redshift-s3-access-role'
+--delimiter '|' gzip;
+
+copy assignment.supplier_stage
+from 'emr://j-35YPEDUMMF9AO/output_store/supplier/20200608/*' credentials
+iam_role 'arn:aws:iam::048532184061:role/aws-assignment-redshift-s3-access-role'
 delimiter '|' gzip;
 
 
